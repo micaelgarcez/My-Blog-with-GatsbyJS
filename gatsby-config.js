304159ceff388fc1bd8e6e108c17db0d9255898a
micaelgarcez <micaelgarcez@outlook.com>
2020-06-12 21:57:38 -0300

    Clear Starter Gatsby Project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My Blog`,
+    description: `learning by doing`,
+    author: `@micael_garcez`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
